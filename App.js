f58ac7b4ca906b95927476e2a8fa2c8b4b4d31ae
Jesse Pollak <jesse@pollak.io>
2016-11-23 14:44:03 -0500

    feat(library): add initial version of library

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Instant2FAComponent from './src/Instant2FAComponent.jsx';
 
-const App = () => (
-  <div>
-    <Instant2FAComponent />
-  </div>
-);
+const App = () => {
+  const onError = (e) => {
+    alert("There was an error: " + e.message)
+  }
+
+  return (
+    <div>
+      <Instant2FAComponent
+        uri='https://hosted.instant2fa.com/users/123456'
+        onError={onError}
+      ></Instant2FAComponent>
+    </div>
+  )
+};
 
 export default App;
--- src/Instant2FAComponent.jsx
+++ src/Instant2FAComponent.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import scriptLoader from 'react-async-script-loader'
 
-const Instant2FAComponent = () => <div>Instant2fa-component</div>;
+class Instant2FAComponent extends Component {
 
-export default Instant2FAComponent;
+  componentWillReceiveProps ({ isScriptLoaded, isScriptLoadSucceed }) {
+    if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished
+      if (isScriptLoadSucceed) {
+        this.init()
+      } else this.props.onError()
+    }
+  }
+
+  componentDidMount () {
+    const { isScriptLoaded, isScriptLoadSucceed } = this.props
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      this.init()
+    }
+  }
+
+  init() {
+    window.Instant2FAPage.configure({
+      element: ReactDOM.findDOMNode(this),
+      uri: this.props.uri
+    }, this.props.onEvent)
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+Instant2FAComponent.propTypes = {
+  uri: React.PropTypes.string.isRequired,
+  onError: React.PropTypes.func,
+  onEvent: React.PropTypes.func
+}
+Instant2FAComponent.SCRIPT_URL = 'https://js.instant2fa.com/hosted.js'
+
+export default scriptLoader([Instant2FAComponent.SCRIPT_URL])(Instant2FAComponent)
